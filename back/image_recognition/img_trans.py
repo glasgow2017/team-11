03a83f1e19a5cc74c0a0df67060fc62cb57b1785
SheriXue <11027605+sherixue@users.noreply.github.com>
2017-11-04 16:58:24 +0000

    translate image into sentences

--- back/image_recognition/img_trans.py
+++ back/image_recognition/img_trans.py
@@ -29,40 +29,53 @@ config = {'location': 'westeurope',
           'visualFeatures': 'Tags,Description'}
 azure_url = url_format.format(location=config.get('location'),
                               vf=config.get('visualFeatures'))
-print azure_url
 
 
-# In[67]:
+# In[77]:
 
 HEADER_CONFIG = {
-    'Ocp-Apim-Subscription-Key': AZURE_APIKEY,
-    'Content-Type' : 'application/json'
+    'url_image': {
+        'Ocp-Apim-Subscription-Key': AZURE_APIKEY,
+        'Content-Type' : 'application/json'
+    },
+    'raw_image' : {
+        'Ocp-Apim-Subscription-Key': AZURE_APIKEY,
+        'Content-Type' : 'application/octet-stream'
+    }
 }
 
 
-# In[68]:
+# In[84]:
 
-def send_request(server_url, img_url, headers=HEADER_CONFIG):
+def send_request_by_url(server_url, img_url, headers=HEADER_CONFIG['url_image']):
     rdata = {'url': img_url}
     r = requests.post(azure_url, headers=headers, data=json.dumps(rdata))
+    return r
+    
+    
+def send_request_by_image(server_url, img_data, headers=HEADER_CONFIG['raw_image']):
+    rdata = {'url': img_url}
+    r = requests.post(azure_url, headers=headers, data=json.dumps(rdata))
+    return r
+
+
+# In[85]:
+
+def parse_response(res):
     try:
-        return r.json()
+        response = res.json()
+        tags = response.get('tags')
+        description = response.get('description', [])
+        captions = description.get('captions', [])
+        tags = {x.get('name'): x.get('confidence') for x in tags}
+        if len(captions) > 0:
+            return captions[0]['text']
     except:
         return {'Error': 'wrong image searching request'}
-#         return {'error': '404'}
 
 
-# In[69]:
+# In[87]:
 
-r = send_request(azure_url, furl, headers=HEADER_CONFIG)
-
-
-# In[70]:
-
-r
-
-
-# In[ ]:
-
-
+r = send_request_by_url(azure_url, furl)
+parse_response(r)
 
