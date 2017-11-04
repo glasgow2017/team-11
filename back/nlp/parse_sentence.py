c5ee108cedd0fb3070a5ae4de46f00a1c1cd66fa
SheriXue <11027605+sherixue@users.noreply.github.com>
2017-11-04 19:08:11 +0000

    convert text into words

--- back/nlp/parse_sentence.py
+++ back/nlp/parse_sentence.py
@@ -3,4 +3,16 @@ import nltk.data
 
 def get_sentences(data):
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    print tokenizer.tokenize(data)
+    return tokenizer.tokenize(data)
+
+def get_words_from_sentence(sentences):
+    regex = re.compile('[^a-zA-Z]')
+    words = [x.split(' ') for x in sentences]
+    words = [x for s in words for x in s]
+    words = [regex.sub('', x) for x in words]
+    words = [x for x in words if len(x) > 0]
+    return words
+
+def get_words_from_article(article):
+    sentences = get_sentences(article)
+    return get_words_from_sentence(sentences)
