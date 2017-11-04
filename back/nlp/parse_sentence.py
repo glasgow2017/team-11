d27478057bf48be3098bf42922f7983d0511af06
SheriXue <11027605+sherixue@users.noreply.github.com>
2017-11-04 20:13:08 +0000

    parse sentences, count words.

--- back/nlp/parse_sentence.py
+++ back/nlp/parse_sentence.py
@@ -16,3 +16,28 @@ def get_words_from_sentence(sentences):
 def get_words_from_article(article):
     sentences = get_sentences(article)
     return get_words_from_sentence(sentences)
+
+
+
+def get_word_bank(words):
+    word_bank = {}
+    for word in words:
+        if word in word_bank:
+            word_bank[word] += 1
+        else:
+            word_bank[word] = 1
+
+    return word_bank
+
+def rank_word_by_appearance(words, order='acs'):
+    word_bank = get_word_bank(words)
+    sorted_word_bank = []
+    for key, value in sorted(word_bank.iteritems(), key=lambda (k,v): (v,k)):
+        sorted_word_bank.append((key, value))
+    if order == 'acs':
+        return sorted_word_bank
+    else:
+        return sorted_word_bank[::-1]
+
+def find_top_words(sorted_word_bank, num=10):
+    return sorted_word_bank[:num]
